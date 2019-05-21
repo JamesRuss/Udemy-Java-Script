13ae72532d796c887c4ba3edab8e87eb2b46749f
James <jamesrussjohnson@gmail.com>
2019-05-20 21:47:24 -0600

    Initial commit

---
+++ Javascript/learn-java.js
@@ -0,0 +1,14 @@
+//var name = "Jack";
+//var age = 23;
+//
+//var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+
+var firstName = "John";
+var lastName = "Johnson";
+var dateOfBirth = "10-2-18";
+var age = 23;
+var profileImgUrl = "http://coolpicks.com/johnjohnson.jpg";
+
+var loginWelcomeMessage = "Welcome, " + firstName + ", here's your news updates.";
+
+console.log(loginWelcomeMessage);
